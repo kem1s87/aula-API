45bc0d539e5e302736a817801159ea87f8d9bf41
kem1s87 <146674265+kem1s87@users.noreply.github.com>
2024-10-14 10:21:39 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const userController = require('./controllers/userController')
-const contentController = require('./controllers/contentController')
-const cardController = require('./controllers/cardController')
+const userController = require('./controllers/userController');
+const contentController = require('./controllers/contentController');
+const cardController = require('./controllers/cardController');
+const deckController = require('./controllers/deckController'); 
 
 const app = express();
 app.use(express.json());
 app.use('/api/users', userController);
 app.use('/api/content', contentController);
 app.use('/api/card', cardController);
-app.use('/api/Deck', ControllersDecks);
-
+app.use('/api/decks', deckController);
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
-})
+});
