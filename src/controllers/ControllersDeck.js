2c14052e4d75d5575dea62d2a0dbc3108db93e81
kem1s87 <146674265+kem1s87@users.noreply.github.com>
2024-10-14 10:22:53 -0300

    Update ControllersDeck.js

--- src/controllers/ControllersDeck.js
+++ src/controllers/ControllersDeck.js
@@ -4,15 +4,14 @@ const authenticateToken = require('../middleware/auth');
 
 const router = express.Router();
 
-router.post('/register', authenticateToken, async(req, res) =>{
-    try{
-        const {text} = req.body;
-        const content = ServicesDeck.createContent(text);
-        res.json(content);
+router.post('/register', authenticateToken, async (req, res) => {
+    try {
+        const { text } = req.body;
+        const content = await ServicesDeck.createDeck(text); 
+        res.status(201).json(content); 
+    } catch (error) {
+        res.status(400).json({ error: error.message }); 
     }
-    catch(error){
-        res.status(60).json({error: error.message});
-    }
-})
+});
 
-module.exports = router;
+module.exports = router; 
